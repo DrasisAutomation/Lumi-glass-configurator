c567e9f12255d95f467f90969470dcd14a0f8ac0
DrasisAutomation <drasisdata@gmail.com>
2025-10-27 11:57:13 +0530

    working code

--- config-admin.js
+++ config-admin.js
@@ -1,6 +1,5 @@
-// config-admin.js - This file should be added to .gitignore
 const ADMIN_CONFIG = {
-    passwordHash: "4ee52e0cb94418d5ea2470b835acc1072b47e5dcde93a29e60a5c1bb15299a62", // Hash of "Dr@sis@utom@tion07"
+    passwordHash: "4ee52e0cb94418d5ea2470b835acc1072b47e5dcde93a29e60a5c1bb15299a62", 
     sessionTimeout: 5 * 60 * 1000, // 5 minutes (short session)
     sessionKey: 'lumi_admin_authenticated',
     maxFailedAttempts: 5,
